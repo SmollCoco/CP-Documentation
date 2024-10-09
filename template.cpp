5655ce39663f914ff7c90e72a70b9f44d048a015
ibhiyassine <72999397+ibhiyassine@users.noreply.github.com>
2024-10-09 01:55:18 +0100

    template
    please if you have any suggestions, tell me.
    I couldn't add all the macros because it depends on the use case ( macros for seg_trees isn't like macros for graphs...)

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define v vector
+#define s set
+#define pq priority_queue
+#define rep(i, a, b, s) for(ll i = (a); i <= (b); i+=(s))
+#define p pair
+#define pb push_back
+#define mk make_pair
+
