c1fee8dda40ed160fccbc126f04e321ab32ed1ac
SmollCoco <144068020+SmollCoco@users.noreply.github.com>
2024-10-09 09:06:48 +0100

    Update Sieve_of_Eratosthenes.cpp

--- Sieve_of_Eratosthenes.cpp
+++ Sieve_of_Eratosthenes.cpp
@@ -1,12 +1,1 @@
-class SieveOfEratosthenes(int n) {
-    public:
-        bool prime[n + 1];
-        memset(prime, true, sizeof(prime));
-    
-        for (int p = 2; p * p <= n; p++) {
-            if (prime[p] == true) {
-                for (int i = p * p; i <= n; i += p)
-                    prime[i] = false;
-            }
-        }
-};
+
